b068849626c206c8f98e09575f04fe4c4f82c390
Joseph Redmond <joseph.conceptualarts@outlook.com>
2024-07-02 13:58:24 -0700

    Initial Commit

---
+++ src/main/java/tech/conceptualarts/openaiintegration/adapter/ChatClientAdapter.java
@@ -0,0 +1,85 @@
+package tech.conceptualarts.openaiintegration.adapter;
+
+import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.stereotype.Component;
+import tech.conceptualarts.logginghelper.adapter.LogAdapter;
+import tech.conceptualarts.openaiintegration.service.ChatClientService;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static tech.conceptualarts.logginghelper.enums.LogType.*;
+
+@Component
+@RequiredArgsConstructor
+public class ChatClientAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(ChatClientAdapter.class);
+    private final String CLASSNAME = ChatClientAdapter.class.getName();
+    private final LogAdapter logAdapter;
+    private final ChatClientService chatClientService;
+
+
+    public Optional<Map<String, Object>> generate(String userInput, Map<String, Object> systemMessageValues) {
+        String methodSignature = "generate(String, Map<String, Object>)";
+        logAdapter.logToFile(LOG, CLASSNAME, methodSignature, ENTER);
+        try {
+
+            if (StringUtils.isBlank(userInput)) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "User Input Cannot Be Null, Blank, Or Only Whitespace");
+                return Optional.empty();
+            } else if (systemMessageValues == null) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "System Message Values Cannot Be Null");
+                return Optional.empty();
+            } else if (systemMessageValues.isEmpty()) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "System Message Values Cannot Be Empty");
+                return Optional.empty();
+            }
+
+            Optional<Message> maybeSystemMessage = chatClientService.getSystemMessage(systemMessageValues);
+            Optional<Message> maybeUserMessage = chatClientService.getUserMessage(userInput);
+
+            if (maybeSystemMessage.isEmpty()) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "System Message Not Found");
+                return Optional.empty();
+            }
+
+            if (maybeUserMessage.isEmpty()) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "User Message Not Found");
+                return Optional.empty();
+            }
+
+            Message systemMessage = maybeSystemMessage.get();
+            Message userMessage = maybeUserMessage.get();
+
+            Optional<Prompt> maybePrompt = chatClientService.createPrompt(userMessage, systemMessage);
+            if (maybePrompt.isEmpty()) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "Prompt Not Found");
+                return Optional.empty();
+            }
+
+            Prompt prompt = maybePrompt.get();
+
+            Optional<ChatResponse> chatResponse = chatClientService.generate(prompt);
+            if (chatResponse.isEmpty()) {
+                logAdapter.logToFile(LOG, CLASSNAME, methodSignature, FAILED, "Chat Response Not Found");
+                return Optional.empty();
+            }
+
+            ChatResponse response = chatResponse.get();
+            Generation generation = response.getResult();
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXIT);
+            return Optional.of(Map.of("generation", generation));
+        } catch (Exception e) {
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXCEPTION, e.getMessage());
+            logAdapter.logToExternal(e);
+            return Optional.empty();
+        }
+    }
+}
---
+++ src/main/java/tech/conceptualarts/openaiintegration/service/ChatClientService.java
@@ -0,0 +1,91 @@
+package tech.conceptualarts.openaiintegration.service;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import tech.conceptualarts.logginghelper.adapter.LogAdapter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static tech.conceptualarts.logginghelper.enums.LogType.*;
+
+@Service
+@RequiredArgsConstructor
+public class ChatClientService {
+
+    @Value("classpath:/prompts/system-message.st")
+    private Resource systemResource;
+    private final Logger LOG = LoggerFactory.getLogger(ChatClientService.class);
+    private final String CLASSNAME = ChatClientService.class.getName();
+    private final LogAdapter logAdapter;
+    private final ChatClient chatClient;
+
+    public Optional<ChatResponse> generate(Prompt prompt) {
+        String methodSignature = "generate(Prompt)";
+        logAdapter.logToFile(LOG, CLASSNAME, methodSignature, ENTER);
+        try {
+            ChatResponse generatedResult = this.chatClient.prompt(prompt).call().chatResponse();
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXIT);
+            return Optional.of(generatedResult);
+        } catch (Exception e) {
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXCEPTION, e.getMessage());
+            logAdapter.logToExternal(e);
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Prompt> createPrompt(Message userMessage, Message systemMessage) {
+        String methodSignature = "createPrompt(Message, Message)";
+        logAdapter.logToFile(LOG, CLASSNAME, methodSignature, ENTER);
+        try {
+            Prompt prompt = new Prompt(List.of(userMessage, systemMessage));
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXIT);
+            return Optional.of(prompt);
+        } catch (Exception e) {
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXCEPTION, e.getMessage());
+            logAdapter.logToExternal(e);
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Message> getUserMessage(String userInput) {
+        String methodSignature = "getUserMessage(String)";
+        logAdapter.logToFile(LOG, CLASSNAME, methodSignature, ENTER);
+        try {
+            Message userMessage = new UserMessage(userInput);
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXIT);
+            return Optional.of(userMessage);
+        } catch (Exception e) {
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXCEPTION, e.getMessage());
+            logAdapter.logToExternal(e);
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Message> getSystemMessage(Map<String, Object> promptValues) {
+        String methodSignature = "getSystemMessage(Map<String, Object>)";
+        logAdapter.logToFile(LOG, CLASSNAME, methodSignature, ENTER);
+        try {
+            SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemResource);
+            Message systemMessage = systemPromptTemplate.createMessage(promptValues);
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXIT);
+            return Optional.of(systemMessage);
+        } catch (Exception e) {
+            logAdapter.logToFile(LOG, CLASSNAME, methodSignature, EXCEPTION, e.getMessage());
+            logAdapter.logToExternal(e);
+            return Optional.empty();
+        }
+    }
+
+}
